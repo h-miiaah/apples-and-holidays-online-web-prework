9b700d17c63506579afb851cb6d264caafbb7c30
Haim D <haim11293@aol.com>
2019-07-02 00:54:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,15 +50,12 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash.each do |season, holiday|
-    puts season.to_s.capitalize! << ":"
-    holiday.each do |holiday, supplies|
-      supplies = supplies.join(", ")
-      holiday = holiday.to_s.split(" ").map {|word| word.capitalize}.join(" ") << ":"
-      puts " " + holiday + " " + supplies
-    end
+  winter_supplies = holiday_hash[:winter].map do |holiday, items|
+    items
   end
+  winter_supplies.flatten
 end
+  
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -68,6 +65,15 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize! << ":"
+    holiday.each do |holiday, supplies|
+      supplies = supplies.join(", ")
+      holiday = holiday.to_s.split(" ").map {|word| word.capitalize}.join(" ") << ":"
+      puts " " + holiday + " " + supplies
+    end
+  end
+end
 
 end
 
